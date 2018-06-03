d812500edf44f0d928c6c02257c357f3e115db38
blackjok3r <blackjok3r@gmail.com>
2018-06-03 22:00:06 +0800

    modified:   komodod/blocknotify.sh

--- komodod/blocknotify.sh
+++ komodod/blocknotify.sh
@@ -35,19 +35,19 @@ if [ $HEIGHT == 10 ] && [[ $TXBLASTER == 1 || $TXBLASTER == 2 ]]
     ./TxBlast 1 &
 fi
 
-if [ $HEIGHT -eq 65 ] && [ $TXBLASTER -eq 1 ]
+if [ $HEIGHT -eq 60 ] && [ $TXBLASTER -eq 1 ]
   then
     TXID=$(komodo-cli -ac_name=$chain sendtoaddress $address $amount)
     echo "TXID=$TXID" > TXID
 fi
 
-if [ $HEIGHT -eq 70 ] && [ $TXBLASTER -eq 2 ]
+if [ $HEIGHT -eq 65 ] && [ $TXBLASTER -eq 2 ]
   then
     TXID=$(komodo-cli -ac_name=$chain sendtoaddress $address $amount)
     echo "TXID=$TXID" > TXID
 fi
 
-if [ $HEIGHT == 75 ] && [[ $TXBLASTER == 1 || $TXBLASTER == 2 ]]
+if [ $HEIGHT == 69 ] && [[ $TXBLASTER == 1 || $TXBLASTER == 2 ]]
   then
     #Start the blaster, $1 specifies amountof payments,options are 1 and 100.
     ./TxBlast 100 &
